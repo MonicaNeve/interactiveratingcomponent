82ae240f7bc0916fee4d6cdd2feae6f5d1f51e9a
MONICA <138527753+MonicaNeve@users.noreply.github.com>
2023-08-15 17:36:25 -0300

    Correção

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-let howcontainernumber = document.querySelector(".how_container_number") 
-let thankyouresultfour = document.querySelector(".thankyou_result-four ")
-let howcontainerbutton = document.querySelector(".how_container_button")
-let howcontainer = document.querySelector(".how_container")
-let thankyoucontainer = document.querySelector(".thank_you_container")
+let howContainerNumber = document.querySelector(".how_container_number") 
+let thankYouResultFour = document.querySelector(".thankyou_result-four ")
+let howContainerButton = document.querySelector(".how_container_button")
+let howContainer = document.querySelector(".how_container")
+let thankYouContainer = document.querySelector(".thank_you_container")
 
-howcontainernumber.addEventListener("click", (event)=>{
-    let  Selecthownumber = event.target.innerText
-    thankyouresultfour.innerText = Selecthownumber
-    if (Selecthownumber >0 || Selecthownumber <=5){
-        howcontainerbutton.addEventListener("click", ()=>{
-    howcontainer.style.display = "none"
-    thankyoucontainer.style.display = "flex"
+howContainerNumber.addEventListener("click", (event)=>{
+    let  SelecThowNumber = event.target.innerText
+    thankYouResultFour.innerText = SelecThowNumber
+    if (SelecThowNumber >0 || SelecThowNumber <=5){
+        howContainerButton.addEventListener("click", ()=>{
+    howContainer.style.display = "none"
+    thankYouContainer.style.display = "flex"
 })
     }
 })
